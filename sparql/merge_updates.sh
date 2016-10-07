2e0123bec684d66a5d8eaa970df2b5bcad28fd34
Jindrich Mynarz <mynarzjindrich@gmail.com>
2016-10-07 17:28:22 +0200

    Merge SPARQL updates

---
+++ sparql/merge_updates.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+set -e
+
+declare -a UPDATES=(
+  # Remove data
+  "delete_empty_lots.ru"
+  "remove_invalid.ru"
+
+  # Clean literals
+  "unquote_literals.ru"
+  "canonicalize_organization_names.ru"
+  "trim_trailing_slash_from_urls.ru"
+
+  # Move properties
+  "invert_is_lot_of.ru"
+  "move_award_date_to_awarded_tender.ru"
+
+  # Merging
+  "blank_nodes_to_hash_iris_postal_addresses.ru"
+  "merge_postal_addresses.ru"
+  "blank_nodes_to_hash_iris_places.ru"
+  "merge_places.ru"
+  "blank_nodes_to_hash_iris_concepts.ru"
+  "merge_concepts.ru"
+  "blank_nodes_to_hash_iris_price_specifications.ru"
+  "merge_price_specifications.ru"
+  "blank_nodes_to_hash_iris_projects.ru"
+  "merge_projects.ru"
+  "blank_nodes_to_hash_iris_criterion_weightings.ru"
+  "merge_criterion_weightings.ru"
+  "blank_nodes_to_hash_iris_award_criteria_combinations.ru"
+  "merge_award_criteria_combinations.ru"
+  "blank_nodes_to_hash_iris_organizations.ru"
+  "merge_organizations.ru"
+  "blank_nodes_to_hash_iris_tenders.ru"
+  "merge_tenders.ru"
+
+  # Reconciliations
+  "reconcile_authority_kinds.ru"
+  "reconcile_contract_kinds.ru"
+  "reconcile_contracting_authority_activities.ru"
+  "reconcile_procedure_types.ru"
+  "reconcile_service_category.ru"
+
+  # Linking
+  "map_cpv_2003_to_cpv_2008.ru"
+  "link_eu_projects.ru"
+)
+
+for update in "${UPDATES[@]}"; do
+  cat $update
+  echo ";"
+done
