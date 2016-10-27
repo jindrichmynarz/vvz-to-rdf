33850ae3bffe0b9d9cf1c29f78764c055e576530
Jindrich Mynarz <mynarzjindrich@gmail.com>
2016-10-27 11:19:29 +0200

    Enlist new updates

--- sparql/merge_transforms.sh
+++ sparql/merge_transforms.sh
@@ -50,10 +50,13 @@ declare -a UPDATES=(
   "create_ico_based_iris.ru"
   "blank_nodes_to_hash_iris_organizations.ru"
   "merge_organizations.ru"
+  "domains_to_ico.ru"
+  "group_organizations_by_name_and_postal_code.ru"
   "blank_nodes_to_hash_iris_tenders.ru"
   "merge_tenders.ru"
   "merge_contracts_without_lots_with_their_lot.ru"
   "cast_single_lots_as_contracts.ru"
+  "remove_organization_data_retrievable_from_ares.ru"
 )
 
 for update in "${UPDATES[@]}"; do
