9ce52087fe8d27c168ca1b508557b797cbb3662a
Jindrich Mynarz <mynarzjindrich@gmail.com>
2017-02-01 12:14:00 +0100

    Use UUID IRIs instead of large hashes

--- sparql/merge_transforms.sh
+++ sparql/merge_transforms.sh
@@ -85,9 +85,9 @@ declare -a UPDATES=(
   "merge_contracts_without_lots_with_their_lot.ru"
   "cast_single_lots_as_contracts.ru"
   "remove_organization_data_retrievable_from_ares.ru"
-  "blank_nodes_to_hash_iris_postal_addresses_all.ru"
-  "blank_nodes_to_hash_iris_concepts_all.ru" # Needs very small page sizes, such as 5.
-  "blank_nodes_to_hash_iris_organizations_all.ru" # Needs very small page sizes, such as 5.
+  "blank_nodes_to_uuid_iris_postal_addresses_all.ru"
+  "blank_nodes_to_uuid_iris_concepts_all.ru"
+  "blank_nodes_to_uuid_iris_organizations_all.ru"
 
   # Normalization
   "convert_eur_to_czk.ru"
